1e9c4fee4f9484ab2d38303bb1cab30c74ed3aa4
Kol Ratner <60704569+kol-ratner@users.noreply.github.com>
2024-11-06 11:21:17 +0200

    updating asg policy

--- terraform/web-server.tf
+++ terraform/web-server.tf
@@ -19,9 +19,10 @@ resource "aws_autoscaling_group" "web_asg" {
 }
 
 resource "aws_autoscaling_policy" "scale_up" {
-  name                   = "scale_up"
-  policy_type            = "TargetTrackingScaling"
-  autoscaling_group_name = aws_autoscaling_group.web_asg.name
+  name                      = "scale_up"
+  policy_type               = "TargetTrackingScaling"
+  autoscaling_group_name    = aws_autoscaling_group.web_asg.name
+  estimated_instance_warmup = 120
 
   target_tracking_configuration {
     predefined_metric_specification {
