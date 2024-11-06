13ebd8aeb99cf8030aa596f0405d79d8a860c9ce
Kol Ratner <60704569+kol-ratner@users.noreply.github.com>
2024-11-06 10:28:02 +0200

    fix

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -41,7 +41,7 @@ resource "aws_lb_listener" "https" {
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = aws_acm_certificate_validation.alb.certificate_arn
+  certificate_arn   = aws_acm_certificate.alb.arn
 
   default_action {
     type             = "forward"
