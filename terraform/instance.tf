e23b2158e5d74c1305d5d38ed27de463371dddb4
Kol Ratner <60704569+kol-ratner@users.noreply.github.com>
2024-11-06 08:47:34 +0200

    adding target group for alb

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -8,6 +8,29 @@ resource "aws_lb" "web_alb" {
   subnets            = [aws_subnet.public_1.id, aws_subnet.public_2.id]
 }
 
+resource "aws_lb_target_group" "web_tg" {
+  name     = "web-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.primary.id
+
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    interval            = 30
+    matcher             = "200"
+    path                = "/"
+    port                = "traffic-port"
+    protocol            = "HTTP"
+    timeout             = 5
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "web-target-group"
+  }
+}
+
 # Auto Scaling Group
 resource "aws_autoscaling_group" "web_asg" {
   desired_capacity    = 2
