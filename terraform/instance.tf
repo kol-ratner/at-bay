90980f4b5906ce94c7c371f48850cfa52390ee25
Kol Ratner <60704569+kol-ratner@users.noreply.github.com>
2024-11-06 08:38:49 +0200

    adding compute to serve our web app

---
+++ terraform/instance.tf
@@ -0,0 +1,41 @@
+
+# Application Load Balancer
+resource "aws_lb" "web_alb" {
+  name               = "hello-world-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+}
+
+# Auto Scaling Group
+resource "aws_autoscaling_group" "web_asg" {
+  desired_capacity    = 2
+  max_size            = 4
+  min_size            = 1
+  target_group_arns   = [aws_lb_target_group.web_tg.arn]
+  vpc_zone_identifier = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+
+  launch_template {
+    id      = aws_launch_template.web_lt.id
+    version = "$Latest"
+  }
+}
+
+# Launch Template
+resource "aws_launch_template" "web_lt" {
+  name_prefix            = "web-"
+  image_id               = "ami-0c55b159cbfafe1f0"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.ec2.id]
+
+  user_data = base64encode(<<-EOF
+    #!/bin/bash
+    yum update -y
+    yum install -y httpd
+    systemctl start httpd
+    systemctl enable httpd
+    echo "<h1>Hello World!</h1>" > /var/www/html/index.html
+    EOF
+  )
+}
