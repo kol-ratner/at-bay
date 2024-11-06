668847494babe090618a695f8fe3a302b42ce758
Kol Ratner <60704569+kol-ratner@users.noreply.github.com>
2024-11-06 12:48:55 +0200

    breaking down cloud resources

--- terraform/web-server.tf
+++ terraform/web-server.tf
@@ -1,63 +1,63 @@
-# Auto Scaling Group
-resource "aws_autoscaling_group" "web_asg" {
-  desired_capacity    = 2
-  max_size            = 4
-  min_size            = 1
-  target_group_arns   = [aws_lb_target_group.web_tg.arn]
-  vpc_zone_identifier = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+# # Auto Scaling Group
+# resource "aws_autoscaling_group" "web_asg" {
+#   desired_capacity    = 1
+#   max_size            = 4
+#   min_size            = 1
+#   target_group_arns   = [aws_lb_target_group.web_tg.arn]
+#   vpc_zone_identifier = [aws_subnet.private_1.id, aws_subnet.private_2.id]
 
-  launch_template {
-    id      = aws_launch_template.web_lt.id
-    version = "$Latest"
-  }
+#   launch_template {
+#     id      = aws_launch_template.web_lt.id
+#     version = "$Latest"
+#   }
 
-  tag {
-    key                 = "Name"
-    value               = "web-asg"
-    propagate_at_launch = true
-  }
-}
+#   tag {
+#     key                 = "Name"
+#     value               = "web-asg"
+#     propagate_at_launch = true
+#   }
+# }
 
-resource "aws_autoscaling_policy" "scale_up" {
-  name                      = "scale_up"
-  policy_type               = "TargetTrackingScaling"
-  autoscaling_group_name    = aws_autoscaling_group.web_asg.name
-  estimated_instance_warmup = 120
+# resource "aws_autoscaling_policy" "scale_up" {
+#   name                      = "scale_up"
+#   policy_type               = "TargetTrackingScaling"
+#   autoscaling_group_name    = aws_autoscaling_group.web_asg.name
+#   estimated_instance_warmup = 120
 
-  target_tracking_configuration {
-    predefined_metric_specification {
-      predefined_metric_type = "ASGAverageCPUUtilization"
-    }
-    target_value = 60.0
-  }
-}
+#   target_tracking_configuration {
+#     predefined_metric_specification {
+#       predefined_metric_type = "ASGAverageCPUUtilization"
+#     }
+#     target_value = 60.0
+#   }
+# }
 
-# Launch Template
-resource "aws_launch_template" "web_lt" {
-  name_prefix            = "web-"
-  image_id               = "ami-0694d931cee176e7d"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.ec2.id]
-  iam_instance_profile {
-    name = aws_iam_instance_profile.ec2_ssm.name
-  }
+# # Launch Template
+# resource "aws_launch_template" "web_lt" {
+#   name_prefix            = "web-"
+#   image_id               = "ami-0694d931cee176e7d"
+#   instance_type          = "t2.micro"
+#   vpc_security_group_ids = [aws_security_group.ec2.id]
+#   iam_instance_profile {
+#     name = aws_iam_instance_profile.ec2_ssm.name
+#   }
 
-  user_data = base64encode(<<-EOF
-    #!/bin/bash
-    apt-get update
-    apt-get install -y apache2
-    systemctl start apache2
-    systemctl enable apache2
-    mkdir -p /var/www/html
-    echo "<h1>Hello World!</h1>" > /var/www/html/index.html
-    EOF
-  )
+#   user_data = base64encode(<<-EOF
+#     #!/bin/bash
+#     apt-get update
+#     apt-get install -y apache2
+#     systemctl start apache2
+#     systemctl enable apache2
+#     mkdir -p /var/www/html
+#     echo "<h1>Hello World!</h1>" > /var/www/html/index.html
+#     EOF
+#   )
 
-  tag_specifications {
-    resource_type = "instance"
-    tags = {
-      Name = "web-server"
-    }
-  }
-}
+#   tag_specifications {
+#     resource_type = "instance"
+#     tags = {
+#       Name = "web-server"
+#     }
+#   }
+# }
 
